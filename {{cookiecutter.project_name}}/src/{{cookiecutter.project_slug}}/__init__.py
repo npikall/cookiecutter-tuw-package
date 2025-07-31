0279a292af9aeb666e152c6ed73ba78283e9afaf
npikall <nikolas.pikall@tuwien.ac.at>
2025-07-31 20:53:52 +0200

    feat: added greeting message
    the main function is the entrypoint for the cli defined in the
    pyproject.toml

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -2,3 +2,6 @@ from {{ cookiecutter.project_slug }}._version import __commit__
 from {{ cookiecutter.project_slug }}._version import __version__
 
 name = "{{ cookiecutter.project_slug }}"
+
+def main() -> None:
+    print(f"Hello from {name}")
